04fcf78f732bcd671bc6080fd976dfbc16b61288
Julio <jreye087@fiu.edu>
2012-12-19 22:39:06 -0500

    Rename project to miamibeachguide Add app config to version control.

---
+++ seniorproject/src/edu/fiu/cs/seniorproject/config/AppConfig.java
@@ -0,0 +1,12 @@
+package edu.fiu.cs.seniorproject.config;
+
+public class AppConfig {
+	public static final String FB_APP_ID = "378790605525916";
+    public static final String GOOGLE_PLACE_APP_ID = "AIzaSyAT2DI3W6uSn6PKX1sYWdf4WSdBa2p4hgc";
+    public static final String EVENTFUL_APP_ID = "HrsPRcW3W49b6hZq";
+    public static final String MBVCA_APP_ID = "";
+    public static final String MBVCA_TOKEN = "122dd08d87393fc34e354dccc4d3458fa2155461";
+    public static final String MBVCA_TOKEN_SECRET = "7de53ebcd686741c50a9f6bc5831b429da21e38a";
+    public static final String TWITTER_CONSUMER_KEY = "B1ky3nHZMqNaC5dGmUDJw";
+	public static final String TWITTER_CONSUMER_SECRET = "QrqAxc6NAONAELLK9ZrD9fWXSd9TSCf3fjXoVSBnk";
+}
